a1db3a0e7e3a7821dc06433bea22802aa873f548
nemesisrobot <diegolopestec2@gmail.com>
2021-09-22 10:03:58 -0300

    arquivo

---
+++ script/constantes_sistema.py
@@ -0,0 +1,7 @@
+#dados para requisição
+HEADER = {
+     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
+    }
+
+URL_FUNDS_EXPLORER = "https://www.fundsexplorer.com.br/funds"
+URL_FUNDAMENTUS = "https://www.fundamentus.com.br/detalhes.php?papel="
